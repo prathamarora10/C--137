41308b07ef701b5c7e87f970378b3a2f1feca600
prathamarora10 <72607002+prathamarora10@users.noreply.github.com>
2022-01-11 19:30:17 +0530

    Add files via upload

---
+++ details.js
@@ -0,0 +1,118 @@
+import React, { Component } from "react"
+import { View, Text, StyleSheet, Button, Alert } from 'react-native'
+import { Card, Icon } from 'react-native-elements'
+import axios from 'axios'
+
+export default class DetailsScreen extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            details : {},
+            imagePath : "",
+            url : 'http://localhost:5000/planet?name=${this.props.navigation.getParam("planet_name")}'
+        }
+    }
+
+    componentDidMount(){
+        this.getPlanets()
+    }
+
+    getPlanets = () => {
+        const { url } = this.state
+        axios.get(url).then(response => {
+            this.setDetails(response.data.data)
+        })
+        .catch(error => {
+            Alert.alert(error.message)
+        })
+    }
+
+    setDetails = (planet_details) => {
+        const planet_type = planet_details.planet_type
+        var imagePath = ""
+        switch(planet_type){
+            case "Gas Giant" : imagePath = require("../assets/gas_giant.png")
+            break
+            case "Neptune Like" : imagePath = require("../assets/neptune_like.png")
+            break
+            case "Terrestrial" : imagePath = require("../assets/terrestrial.png")
+            break
+            case "Super Earth" : imagePath = require("../assets/super_earth.png")
+            break
+        }
+        this.setState({
+            details : planet_details,
+            imagePath : imagePath
+        })
+    }
+
+    render(){
+        const { details, imagePath } = this.state
+        if(details.specifications){
+            return(
+                <View style={styles.container}>
+                    <Card
+                    title={details.name}
+                    image={imagePath}
+                    imageProps={{
+                        resizeMode : "contain",
+                        width : "100%",
+                    }}
+                    >
+                        <View>
+                            <Text style={styles.cardItem}>
+                                { `Distance from Earth : ${details.distance_from_earth}`}
+                            </Text>
+                            <Text style={styles.cardItem}>
+                                { `Distance from Sun : ${details.distance_from_their_sun}`}
+                            </Text>
+                            <Text style={styles.cardItem}>
+                                { `Gravity : ${details.gravity}`}
+                            </Text>
+                            <Text style={styles.cardItem}>
+                                { `Orbital Period : ${details.orbital_period}`}
+                            </Text>
+                            <Text style={styles.cardItem}>
+                                { `Orbital Speed : ${details.orbital_speed}`}
+                            </Text>
+                            <Text style={styles.cardItem}>
+                                { `Planet Mass : ${details.planet_mass}`}
+                            </Text>
+                            <Text style={styles.cardItem}>
+                                { `Planet radius : ${details.planet_radius}`}
+                            </Text>
+                            <Text style={styles.cardItem}>
+                                { `Planet Type : ${details.planet_type}`}
+                            </Text>
+                        </View>
+                        <View>
+                            <Text style={styles.cardItem}>
+                                { details.specifications?`Specifications : ` : ""}
+                            </Text>
+                           { details.specifications.map((item, index) => (
+                               <Text
+                               key={index.toString()}
+                               style={{
+                                   marginLeft : 50
+                               }}
+                               >
+                                   { item }
+                               </Text>
+                           )) }
+                        </View>
+                    </Card>
+                </View>
+            )
+        }
+        return null
+    }
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1
+    },
+    cardItem : {
+        marginBottom : 10
+    }
+})
